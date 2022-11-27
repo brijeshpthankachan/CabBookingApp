75df79e976e3aea3ca2f1a8bd042b3e3e59a10d2
Yi-Sunshin <brijeshpthankachan@gmail.com>
2022-11-27 23:38:14 +0530

    User Home Partial

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ public class ApplicationDbContext :  IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)   
     {
+     
         
     }
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
